cddfe56e0e8fd480e773013466ee22091b57027c
TGDevourer <jetze.luyten@hotmail.xn--co-pka>
2023-10-18 14:31:51 +0200

    Added security...I think

---
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package fact.it.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers(HttpMethod.GET, "/tables")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated()
+                )
+                .oauth2ResourceServer((oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                ));
+        return serverHttpSecurity.build();
+    }
+}
