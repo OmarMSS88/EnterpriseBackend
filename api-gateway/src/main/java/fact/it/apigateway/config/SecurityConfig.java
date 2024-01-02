3d10fae2d2635b96b532df2a93d5098178bdd2e2
Jetze Luyten <r0785521@student.thomasmore.be>
2024-01-02 19:57:08 +0100

    Trying to fix api-gateway

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -3,27 +3,46 @@ package fact.it.apigateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
                 .authorizeExchange(exchange ->
-                        exchange
-                                .pathMatchers(HttpMethod.GET, "/tables").permitAll()
-                                .pathMatchers(HttpMethod.GET, "/visits").permitAll()
-                                .pathMatchers(HttpMethod.GET, "/waiters").permitAll()
-                                .pathMatchers(HttpMethod.GET, "/dishes").permitAll()
-                                .anyExchange().authenticated()
+                        exchange.pathMatchers(HttpMethod.GET, "/tables")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
-
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers(HttpMethod.GET, "/visits")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated()
+                )
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers(HttpMethod.GET, "/waiters")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated()
+                )
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers(HttpMethod.GET, "/dishes")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated()
+                )
+                .oauth2ResourceServer((oauth2 -> oauth2
+                        .jwt(withDefaults())
+                ));
         return serverHttpSecurity.build();
     }
 }
