f9fc3cff5e89c5247b17c5fd00a73feb5272b56f
Jetze Luyten <r0785521@student.thomasmore.be>
2024-01-04 14:59:32 +0100

    Adding data to db

--- table-service/src/main/java/fact/it/tableservice/service/TableService.java
+++ table-service/src/main/java/fact/it/tableservice/service/TableService.java
@@ -29,8 +29,8 @@ public class TableService {
     @PostConstruct
     private void postConstruct() {
         Table table = new Table("A1", "A1", 2);
-        Table B1 = new Table("A1", "B2", 4);
-        Table C4 = new Table("A1", "C4", 8);
+        Table B1 = new Table("A2", "B2", 4);
+        Table C4 = new Table("A3", "C4", 8);
         tableRepository.save(table);
         tableRepository.save(B1);
         tableRepository.save(C4);
