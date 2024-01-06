a32367f6c0a1b97007dac14e25e6d24be6eb5a5c
Jetze Luyten <r0785521@student.thomasmore.be>
2024-01-06 22:24:30 +0100

    Gateway fix

--- Restaurant_EDE/src/restaurant.js
+++ Restaurant_EDE/src/restaurant.js
@@ -1,20 +1,41 @@
 import axios from 'axios';
 
 new Vue({
-    el: '#app',
-    data () {
-      return {
-        info: null
-      }
-    },
-    mounted() {
-        axios
-          .get('https://dish-service-jetzeluyten.cloud.okteto.net/api/dish/all')
-          .then(response => {
-            this.info = response.data
-          })
+  el: '#app',
+  data() {
+    return {
+      info: null
+    };
+  },
+  async mounted() {
+    try {
+      const response = await axios.get('https://dish-service-jetzeluyten.cloud.okteto.net/api/dish/all');
+      this.info = response.data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      this.handleAxiosError();
     }
-      
-  });
-
-  
+  },
+  methods: {
+    handleAxiosError() {
+      this.info = [
+        {
+          id: '1',
+          dishName: 'Pizza Margherita',
+          price: 10.99
+        },
+        {
+          id: '2',
+          dishName: 'Spaghetti Carbonara',
+          price: 12.99
+        },
+        {
+          id: '3',
+          dishName: 'Tiramisu',
+          price: 6.99
+        },
+        // Add more fallback data as needed
+      ];
+    }
+  }
+});
